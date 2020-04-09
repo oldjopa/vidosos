6b289c66db4b0012c9036798b8fd148f8b833914
FlintPQ <57351216+FlintPQ@users.noreply.github.com>
2020-04-09 19:34:42 +1000

    added reg

--- data/forms.py
+++ data/forms.py
@@ -1,1 +1,14 @@
-# forms
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, BooleanField, SubmitField, PasswordField, IntegerField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired
+
+
+class RegisterForm(FlaskForm):
+    login = StringField('Имя пользователя', validators=[DataRequired()])
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
+    gender = StringField('Имя пользователя', validators=[DataRequired()])
+    age = IntegerField(u'Age')
+    submit = SubmitField('Войти')
