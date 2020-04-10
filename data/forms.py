4128083c19fc10c54a5a090bcafb814845c5d7fe
FlintPQ <57351216+FlintPQ@users.noreply.github.com>
2020-04-10 14:43:33 +1000

    add_forms

--- data/forms.py
+++ data/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, BooleanField, SubmitField, PasswordField, IntegerField
+from wtforms import StringField, TextAreaField, BooleanField, SubmitField, PasswordField, IntegerField, FileField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired
 
@@ -11,4 +11,16 @@ class RegisterForm(FlaskForm):
     password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
     gender = StringField('Имя пользователя', validators=[DataRequired()])
     age = IntegerField(u'Age')
+    submit = SubmitField('Зарегистрироваться')
+
+
+class LoginForm(FlaskForm):
+    login = StringField('Имя пользователя', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
     submit = SubmitField('Войти')
+
+
+class AddVideo(FlaskForm):
+    file = FileField()
+    description = StringField('описание')
+    submit = SubmitField('загрузить')
