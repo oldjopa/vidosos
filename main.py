2570920f77aff500295c041edd2711630773f1c5
Terminika <veronikadorohovitch@yandex.ru>
2020-04-24 17:04:25 +1000

    deleting and viewing own and favourite videos, association table (own_video, favourite_video) isn't in project. (wip)

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from flask_login.utils import *
 from data.forms import *
 from data.user import User
 from data.video import Video
+import vidosos_api
 
 UPLOAD_FOLDER = './videos'
 
@@ -28,29 +29,6 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        session = db_session.create_session()
-        user = session.query(User).filter(User.login == form.login.data).first()
-        session.commit()
-        if user and user.check_password(form.password.data):
-            login_user(user, remember=form.remember_me.data)
-            return redirect("/")
-        return render_template('user_log.html', title='Авторизация',
-                               message="Неправильный логин или пароль",
-                               form=form)
-    return render_template('user_log.html', title='Авторизация', form=form)
-
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect("/")
-
-
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
@@ -65,10 +43,10 @@ def register():
                                    form=form,
                                    message="Такой пользователь уже есть")
         user = User(
-            login=form.login,
-            age=form.age,
-            gender=form.gender,
-            email=form.email
+            login=form.login.data,
+            age=form.age.data,
+            gender=form.gender.data,
+            email=form.email.data
         )
         user.set_password(form.password.data)
         session.add(user)
@@ -93,6 +71,59 @@ def login():
     return render_template('user_log.html', title='Авторизация', form=form)
 
 
+@app.route('/profile')
+def profile():
+    return render_template('user.html')
+
+
+@app.route('/my_videos')
+def get_user_videos():
+    session = db_session.create_session()
+    users_videos = session.query(Video).filter(
+        (own_video.c.user_id == current_user.id)
+        & (Video.id == own_video.c.video_id)).all()
+    session.commit()
+    return render_template('view_videos.html', title='Мои видео',
+                           videos=users_videos)
+
+
+@app.route('/favourite_videos')
+def get_favourite_videos():
+    session = db_session.create_session()
+    users_videos = session.query(Video).filter(
+        (favourite_video.c.user_id == current_user.id)
+        & (Video.id == favourite_video.c.video_id)).all()
+    session.commit()
+    return render_template('view_videos.html', title='Избранные видео',
+                           videos=users_videos)
+
+
+@app.route('/delete_my_video/<video_id>')
+def delete_my_video(video_id):
+    session = db_session.create_session()
+    video = session.query(Video).filter(Video.id == video_id).first()
+    if video:
+        session.delete(video)
+        session.commit()
+    else:
+        abort(404)
+    session.commit()
+    return redirect('/my_videos')
+
+
+@app.route('/delete_favourite_video/<video_id>')
+def delete_my_video(video_id):
+    session = db_session.create_session()
+    video = session.query(Video).filter(Video.id == video_id).first()
+    if video:
+        session.delete(video)
+        session.commit()
+    else:
+        abort(404)
+    session.commit()
+    return redirect('/favourite_videos')
+
+
 @app.route('/logout')
 @login_required
 def logout():
@@ -102,6 +133,7 @@ def logout():
 
 def main():
     db_session.global_init("db/videos.sqlite")
+    app.register_blueprint(vidosos_api.blueprint)
     app.run()
 
 
