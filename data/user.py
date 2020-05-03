04f09951381a8fcb92774ae6c473c0652fc3d36f
lolkek22854 <50008736+lolkek22854@users.noreply.github.com>
2020-05-03 11:46:03 +1000

    Added email veifycation

--- data/user.py
+++ data/user.py
@@ -14,6 +14,8 @@ class User(SqlAlchemyBase, UserMixin):
     age = sqlalchemy.Column(sqlalchemy.Integer)
     gender = sqlalchemy.Column(sqlalchemy.String)
     email = sqlalchemy.Column(sqlalchemy.String)
+    is_active = sqlalchemy.Column(sqlalchemy.Boolean)
+    act_id = sqlalchemy.Column(sqlalchemy.String)
     role = sqlalchemy.Integer
     own_videos = orm.relation("Video", secondary="own_video_association")
     viewed_videos = orm.relation("Video",
---
+++ send_mail.py
@@ -0,0 +1,29 @@
+import smtplib
+from email.message import EmailMessage
+from email.mime.text import MIMEText
+
+def send_mail(to, href):
+    smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
+    smtpObj.connect("smtp.gmail.com", 587)
+    smtpObj.ehlo()
+    smtpObj.starttls()
+    smtpObj.ehlo()
+
+    textfile='templates/mail.html'
+    with open(textfile) as fp:
+        # Create a text/plain message
+        msg = EmailMessage()
+        content = fp.read()
+        content = content % ('http://127.0.0.1:5000/verify/' + href, 'http://127.0.0.1:5000/verify/' + href, 'http://127.0.0.1:5000/verify/' + href)
+        body = MIMEText(content, 'html')
+        msg.set_content(body)
+
+    # me == the sender's email address
+    # you == the recipient's email address
+    msg['Subject'] = f'The contents of {textfile}'
+    msg['From'] = 'lol@lol'
+    msg['To'] = to
+
+    smtpObj.login('vidososverify@gmail.com', 'kek228lol')
+    smtpObj.send_message(msg)
+    smtpObj.quit()
