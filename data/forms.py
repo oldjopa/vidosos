853df8895e489c70758f67341f7d2a39ccf92f58
Terminika <veronikadorohovitch@yandex.ru>
2020-05-07 09:56:35 +1000

    redesign login form

--- data/forms.py
+++ data/forms.py
@@ -5,18 +5,26 @@ from wtforms.validators import DataRequired
 
 
 class RegisterForm(FlaskForm):
-    name = StringField('Your name', validators=[DataRequired()], render_kw={"placeholder": "Your name"})
+    name = StringField('Your name', validators=[DataRequired()],
+                       render_kw={"placeholder": "Your name"})
     age = IntegerField('Age', render_kw={"placeholder": "Age"})
-    login = StringField('Login', validators=[DataRequired()], render_kw={"placeholder": "Login"})
-    email = EmailField('Email', validators=[DataRequired()], render_kw={"placeholder": "Email"})
-    password = PasswordField('Password', validators=[DataRequired()], render_kw={"placeholder": "Password"})
-    password_again = PasswordField('Repeat Password', validators=[DataRequired()], render_kw={"placeholder": "Repeat Password"})
+    login = StringField('Login', validators=[DataRequired()],
+                        render_kw={"placeholder": "Login"})
+    email = EmailField('Email', validators=[DataRequired()],
+                       render_kw={"placeholder": "Email"})
+    password = PasswordField('Password', validators=[DataRequired()],
+                             render_kw={"placeholder": "Password"})
+    password_again = PasswordField('Repeat Password',
+                                   validators=[DataRequired()],
+                                   render_kw={"placeholder": "Repeat Password"})
     submit = SubmitField('Sign Up')
 
 
 class LoginForm(FlaskForm):
-    login = StringField('Login', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    login = StringField('Login', validators=[DataRequired()],
+                        render_kw={"placeholder": "Login"})
+    password = PasswordField('Password', validators=[DataRequired()],
+                             render_kw={"placeholder": "Password"})
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Submit')
 
