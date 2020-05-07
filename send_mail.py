a91c785d33c3b9b2d5021cd5ca0784d7f7c5da22
Veronika <veronikadorohovitch@yandex.ru>
2020-05-07 13:11:44 +1100

    fix bug with rendering mail in yandex mail fix link color in yandex mail

--- send_mail.py
+++ send_mail.py
@@ -1,6 +1,8 @@
 import smtplib
 from email.message import EmailMessage
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.utils import formatdate
 
 def send_mail(to, href):
     smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
@@ -11,18 +13,19 @@ def send_mail(to, href):
 
     textfile='templates/mail.html'
     with open(textfile) as fp:
-        # Create a text/plain message
-        msg = EmailMessage()
+        # Create MIMEMultipart message
+        msg = MIMEMultipart()
         content = fp.read()
-        content = content % ('http://mesenev.ru:1111/verify/' + href, 'http://mesenev.ru:1111/verify/' + href, 'http://mesenev.ru:1111/verify/' + href)
-        # content = content % ('http://127.0.0.1:5000/verify/' + href, 'http://127.0.0.1:5000/verify/' + href, 'http://127.0.0.1:5000/verify/' + href)
+        # content = content % ('http://mesenev.ru:1111/verify/' + href, 'http://mesenev.ru:1111/verify/' + href, 'http://mesenev.ru:1111/verify/' + href)
+        content = content % ('http://127.0.0.1:5000/verify/' + href, 'http://127.0.0.1:5000/verify/' + href, 'http://127.0.0.1:5000/verify/' + href)
         body = MIMEText(content, 'html')
-        msg.set_content(body)
+        msg.attach(body)
 
     # me == the sender's email address
     # you == the recipient's email address
     msg['Subject'] = f'Verify your email'
     msg['From'] = 'verify-vidosos'
+    msg["Date"] = formatdate(localtime=True)
     msg['To'] = to
 
     smtpObj.login('vidososverify@gmail.com', 'kek228lol')
