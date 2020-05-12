a7042ed08a05e5fdcec0235f2b6cf6f00a7c6cfe
Terminika <veronikadorohovitch@yandex.ru>
2020-05-13 08:19:21 +1000

    server redirecting in send mail

--- utils/send_mail.py
+++ utils/send_mail.py
@@ -17,8 +17,8 @@ def send_mail(to, href):
         # Create MIMEMultipart message
         msg = MIMEMultipart()
         content = fp.read()
-        # content = content % ('http://mesenev.ru:1111/verify/' + href, 'http://mesenev.ru:1111/verify/' + href, 'http://mesenev.ru:1111/verify/' + href)
-        content = content % ('http://127.0.0.1:5000/verify/' + href, 'http://127.0.0.1:5000/verify/' + href, 'http://127.0.0.1:5000/verify/' + href)
+        content = content % ('http://mesenev.ru:1111/verify/' + href, 'http://mesenev.ru:1111/verify/' + href, 'http://mesenev.ru:1111/verify/' + href)
+        # content = content % ('http://127.0.0.1:5000/verify/' + href, 'http://127.0.0.1:5000/verify/' + href, 'http://127.0.0.1:5000/verify/' + href)
         body = MIMEText(content, 'html')
         msg.attach(body)
 
